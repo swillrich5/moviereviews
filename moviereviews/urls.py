dfc6c4553457a356f90069c45aaadca6ee667caa
Scott Willrich <swillrich@gmail.com>
2022-11-19 22:25:47 -0600

    set up support for images and included Movie model in admin.py

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 # from movie import views as movieViews
 
 urlpatterns = [
@@ -22,3 +24,6 @@ urlpatterns = [
     # path('', movieViews.home)
     path('', include('movie.urls', namespace='movie')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
