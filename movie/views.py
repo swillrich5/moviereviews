0b95230ef255793d0fbdee43774ebec3ad09fa78
Scott Willrich <swillrich@gmail.com>
2022-11-19 18:26:54 -0600

    set up home page template

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Welcome to Movie Reviews Home Page</h1>')
+    return render(request, 'home.html')
 
 
 def about(request):
