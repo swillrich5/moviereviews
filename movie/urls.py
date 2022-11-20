052e26c4718bfa749e8f3146d2954dda2c872a5a
Scott Willrich <swillrich@gmail.com>
2022-11-19 21:22:08 -0600

    added search form and email signup

--- movie/urls.py
+++ movie/urls.py
@@ -5,5 +5,7 @@ app_name = 'movie'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
+
 ]
--- movie/views.py
+++ movie/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'name': 'Scott Willrich'})
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm': searchTerm})
 
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
 
 def about(request):
     return HttpResponse('<h1>Welcome to Movie Reviews About Page</h1>')
