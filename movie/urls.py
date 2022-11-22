eae7d7f52a9cc2af80a4f85b8e6206c3c605f778
Scott Willrich <swillrich@gmail.com>
2022-11-22 16:22:47 -0600

    create review functionality added

--- movie/urls.py
+++ movie/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('movie/<int:movie_id>', views.movie_detail, name='movie_detail'),
     path('signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
+    path('movie/<int:movie_id>/create/', views.createreview, name="createreview"),
 
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Movie
+from .models import Movie, Review
+from .forms import ReviewForm
 
 # Create your views here.
 def home(request):
@@ -24,3 +25,27 @@ def about(request):
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'movie_detail.html', {'movie': movie})
+
+
+def createreview(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if request.method == 'GET':
+        return render(request, 'createreview.html',
+                        {
+                            'form': ReviewForm(),
+                            'movie': movie
+                        })
+    else:
+        try:
+            form = ReviewForm(request.POST)
+            newReview = form.save(commit=False)
+            newReview.user = request.user
+            newReview.movie = movie
+            newReview.save()
+            return redirect('movie:movie_detail', newReview.movie.id)
+        except ValueError:
+            return render(request, 'createreview.html', 
+                            {
+                                'form': ReviewForm(),
+                                'error': 'bad data passed in'
+                            })
